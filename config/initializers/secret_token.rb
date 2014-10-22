52be60374306f1d83ac62a7d2c2e5461606e223d
Akira Panda <p.chenliang@gmail.com>
2014-10-22 20:04:24 +0800

    init push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ziyougou::Application.config.secret_key_base = '7cd26f27d9f0e6fe645bfcf6eb3266a609b02f2189bfe21009767db35001b3ffc57051b93d5e438340a5b8946ced7ee10d6c4d6bfa5c3cae8d4b1f737af59fcd'
