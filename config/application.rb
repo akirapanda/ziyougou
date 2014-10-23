ae349a74a4a16409c3a4a6b496adbeb20c6fbbe8
Akira Panda <p.chenliang@gmail.com>
2014-10-23 22:25:35 +0800

    add pipeline:

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,7 @@ module Ziyougou
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.assets.precompile += %w(mobile_style.css mobile_style.js)    
+    
   end
 end
