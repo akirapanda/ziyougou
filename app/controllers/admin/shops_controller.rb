04b3315e2592c881bd94115e130e47a167dca91c
Akira Panda <p.chenliang@gmail.com>
2014-10-24 15:03:49 +0800

    Add basic admin/shop list function

--- app/controllers/admin/shops_controller.rb
+++ app/controllers/admin/shops_controller.rb
@@ -1,5 +1,8 @@
 class  Admin::ShopsController < Admin::BaseController  
     def index
+      @q = Shop.search(params[:q])
+      @shops = @q.result(distinct: true)
+      @shops_grid = initialize_grid(@shops,:per_page => 50)
     end
     
 end
